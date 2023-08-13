e5e482245817c5a5b3a39e69b975b1bd6b1f2582
iSz <helio_diniz_junior@hotmail.com>
2023-08-13 03:28:42 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Container from "./components/Container/container";
+import Routers from "./routes";
 
 function App() {
   return (
     <div>
-      <Container />
+      <Routers />
     </div>
   );
 }
